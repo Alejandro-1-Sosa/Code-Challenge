af5f14b1f9cbf63d804134633e4320abcb51bba8
Alejandro Sosa <sosaa9302@gmail.com>
2023-09-07 13:00:34 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
+// Obtiene una referencia al botón con ID "btn"
 let btn = document.getElementById("btn");
 
 btn.addEventListener('click', function(event){
-    event.preventDefault(); //El preventDefault lo ejecuto para
+    event.preventDefault();//El preventDefault lo ejecuto para prevenir el envío predeterminado del formulario (envío de datos y recarga de la pagina)
 
+// Obtener los valores de los campos de entrada por sus respectivos IDs
 let name = document.getElementById("name").value;
 let lastName = document.getElementById("last-name").value;
 let birthdate = document.getElementById("birthdate").value;
 
-
+// Crear un objeto "datos" con los valores obtenidos
 const datos = {
     'name': name,
     'lastName': lastName,
     'birthdate': birthdate
 }
 
+// Realizar una solicitud POST a la API de JSONPlaceholder
 fetch('https://jsonplaceholder.typicode.com/users',{
-    method: 'POST',
+    method: 'POST', 
     headers:{
-        'Content-Type': 'application/json',  
+        'Content-Type': 'application/json',  // Especifica el tipo de contenido JSON
     },
-    body: JSON.stringify(datos)
+    body: JSON.stringify(datos) // Convertir el objeto "datos" a JSON y enviarlo en el cuerpo de la solicitud
 })
-.then(response => response.json())
-.then(data => console.log(datos))
+.then(response => response.json()) // Analiza la respuesta JSON
+.then(data => console.log(datos)) // Imprime los datos enviados en la consola 
 
 });
 
